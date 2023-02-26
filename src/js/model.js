89e4256e4654b2b977e56cbb1300144da876b991
kastastin <kostya242421@gmail.com>
2023-02-26 21:04:41 +0200

    Implementing Search Results - Part 1: SearchView, addHandlerSearch + controlSearchResults

--- src/js/model.js
+++ src/js/model.js
@@ -2,12 +2,16 @@ import { API_URL } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
-  recipe: {}
+  recipe: {},
+  search: {
+    query: '',
+    results: []
+  }
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -20,6 +24,24 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients
     };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map((recipe) => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        image: recipe.image_url
+      };
+    });
   } catch (error) {
     throw error;
   }
