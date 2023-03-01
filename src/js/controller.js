7abf034c6aed2650a36ef806545aacc7bbc08c95
kastastin <kostya242421@gmail.com>
2023-03-01 14:30:19 +0200

    Developing a DOM Updating Algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -14,6 +14,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // Loading Recipe
     await model.loadRecipe(id);
 
@@ -58,7 +61,7 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
