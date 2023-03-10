187dcb5f1ac2ca080771de3c52d534ba1bce406e
kastastin <kostya242421@gmail.com>
2023-03-10 18:29:42 +0200

    fix previewView import

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,8 +2,8 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
-import bookmarksView from './views/bookmarksView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 import { MODAL_CLOSE_SEC } from './config';
 
